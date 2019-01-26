b19906d8df78a9dc638a14b9e477660938a067e9
samstoia <samstoia@gmail.com>
2019-01-26 10:17:39 -0800

    adds JS, for loop, some branching, not functional yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
+//Front End
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var inputNumber = parseInt($("input#input").val());
+    $('#output').text(beepBoop(inputNumber));
+  });
+});
 
+
+//back end
+
+function beepBoop(inputNumber) {
+  var newArray = []
+  for (var i = 0; i <= inputNumber; i++) {
+    if (inputNumber.toString().split().includes("0")) {
+    newArray.push(" boop ")
+    }
+    else if (inputNumber.toString().split().includes("1")) {
+    newArray.push(" Beep ")
+    }
+    else if (i % 3 === 0 && i !== 0) {
+    newArray.push("i'm sorry dave ")
+    }
+    else {
+      newArray.push(i);
+    }
+
+  }
+  return newArray
+
+}
