0704b263123b6165189a88ff95fec98f335d0312
samstoia <samstoia@gmail.com>
2019-01-26 10:27:25 -0800

    Create slightly more functional program, but branching for 0 and 1 still not working correctly

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
 function beepBoop(inputNumber) {
   var newArray = []
   for (var i = 0; i <= inputNumber; i++) {
-    if (inputNumber.toString().split().includes("0")) {
+    if (inputNumber.toString().split(i).includes("0")) {
     newArray.push(" boop ")
     }
-    else if (inputNumber.toString().split().includes("1")) {
+    else if (inputNumber.toString().split(i).includes("1")) {
     newArray.push(" Beep ")
     }
     else if (i % 3 === 0 && i !== 0) {
