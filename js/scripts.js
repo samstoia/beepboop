439d9c69cd30259d7505c15294a4871eebd27fba
samstoia <samstoia@gmail.com>
2019-05-10 22:20:19 -0700

    fix logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Front End
-$(document).ready(function() {
-  $("form#formOne").submit(function(event) {
+$(document).ready(function () {
+  $("form#formOne").submit(function (event) {
     event.preventDefault();
     var inputNumber = parseInt($("input#input").val());
     $('#output').text(beepBoop(inputNumber));
@@ -13,14 +13,13 @@ $(document).ready(function() {
 function beepBoop(inputNumber) {
   var newArray = []
   for (var i = 0; i <= inputNumber; i++) {
-    if (i.toString().split().includes("0")) {
-    newArray.push(" boop ")
+    if (i % 3 === 0 && i !== 0) {
+      newArray.push("i'm sorry dave ")
+    } else if (i.toString().includes("0")) {
+      newArray.push(" boop ")
     }
-    else if (i.toString().split().includes("1")) {
-    newArray.push(" Beep ")
-    }
-    else if (i % 3 === 0 && i !== 0) {
-    newArray.push("i'm sorry dave ")
+    else if (i.toString().includes("1")) {
+      newArray.push(" Beep ")
     }
     else {
       newArray.push(i);
